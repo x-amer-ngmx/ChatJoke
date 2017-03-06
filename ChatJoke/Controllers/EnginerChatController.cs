6d42dc19ce87e06320a7b5ade88bcf9071e53045
x-amer-ngmx <naguwal@gmail.com>
2017-03-06 16:54:17 +0300

    Подготовка чата, включаем в проект Microsoft.Web.WebSockets. Формируем основные классы (копируем из тестового проекта, затем допиливаем до решения поставленой задачи)

--- ChatJoke/Controllers/EnginerChatController.cs
+++ ChatJoke/Controllers/EnginerChatController.cs
@@ -1,17 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
-using System.Web.Mvc;
+using System.Web.Http;
+using Microsoft.Web.WebSockets;
 
 namespace ChatJoke.Controllers
 {
-    public class EnginerChatController : Controller
+    public class EnginerChatController : ApiController
     {
-        // GET: EnginerChat
-        public ActionResult ChatRoom()
+        public HttpResponseMessage Get()
         {
-            return View();
+            HttpContext.Current.AcceptWebSocketRequest(new ChatWebSocketHandler());
+            return Request.CreateResponse(HttpStatusCode.SwitchingProtocols);
+
+        }
+
+        class ChatWebSocketHandler : WebSocketHandler
+        {
+            private static WebSocketCollection _chatCients = new WebSocketCollection();
+            public string uname;
+
+            public ChatWebSocketHandler()
+            {
+                
+            }
+
+            public override void OnOpen()
+            {
+
+                if (!_chatCients.Contains(this))
+                {
+                    uname = Guid.NewGuid().ToString("N");
+                    //this.uname = this.WebSocketContext.QueryString["uname"];
+                    _chatCients.Add(this);
+                    _chatCients.Broadcast($"--- к нам присоеденился пользователь с иминем :{uname} ---");
+                }
+            }
+
+            public override void OnMessage(string message)
+            {
+                if (message.IndexOf(":") >= 0)
+                {
+                    var spl = message.Split(':');
+                    var users_private = _chatCients.Where(r => ((ChatWebSocketHandler)r).uname == spl[0] || ((ChatWebSocketHandler)r).uname == this.uname);
+                    var collect = new WebSocketCollection();
+                    foreach (ChatWebSocketHandler cont in users_private)
+                    {
+                        cont.Send($"{(uname == cont.uname ? $"{uname} -> {spl[0]}" : $"{uname} <- {cont.uname}")}: {spl[1]}");
+                    }
+                    //collect.Broadcast($"{uname}: {spl[1]}"); // .Broadcast($"{uname}: {message}");
+                }
+                else
+                {
+                    _chatCients.Broadcast($"{uname}: {message}");
+                }
+            }
+
+            public override void OnClose()
+            {
+                _chatCients.Remove(this);
+                _chatCients.Broadcast($"Пользователь с именем {uname} покинул чат :(");
+            }
+
         }
     }
 }
---
+++ ChatJoke/Controllers/RunChatController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using ChatJoke.App_Start;
+
+namespace ChatJoke.Controllers
+{
+    public class RunChatController : Controller
+    {
+        public RunChatController()
+        {
+            GlobalConfiguration.Configure(ConfigApi.Register);
+        }
+
+        // GET: RunChat
+        public ActionResult Index()
+        {
+            return null;
+        }
+    }
+}
