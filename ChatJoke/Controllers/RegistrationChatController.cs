e13b52ee33396a1906fba811acf73bcb8a751e73
x-amer-ngmx <naguwal@gmail.com>
2017-03-06 15:45:08 +0300

    Стар проекта

---
+++ ChatJoke/Controllers/RegistrationChatController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatJoke.Controllers
+{
+    public class RegistrationChatController : Controller
+    {
+        // GET: RegestrationChat
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        public ActionResult RegistrationConfirm()
+        {
+            return View();
+        }
+
+        public ActionResult Reminder()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+    }
+}
